6b03a0e271577f92e8fc9b30d824c09866733183
jaykorea <jhjo1997@naver.com>
2022-03-29 14:43:20 +0900

    first commit

---
+++ VES_EV/VES_EV.ino
@@ -0,0 +1,433 @@
+#include <Servo.h>
+
+Servo servo;    // servo motor
+int potPin = 0; // analog pin number that is connected to potentialmeter
+int accel_val;        // analog pin read value
+int pre_accel_val;
+int thresh = 50;
+
+uint32_t PLAYING_TIME = 2500; // playing time
+uint32_t FUELCUT_TIME = 800; //fuelcut time
+
+#define MIN 0
+#define MAX 179
+
+#define mp3 Serial3    // Connect the MP3 Serial Player to the Arduino MEGA Serial3 (14 TX3 -> RX, 15 RX3 -> TX)
+
+static int8_t Send_buf[8] = {0}; // Buffer for Send commands.  // BETTER LOCALLY
+static uint8_t ansbuf[10] = {0}; // Buffer for the answers.    // BETTER LOCALLY
+
+String mp3Answer;           // Answer from the MP3.
+
+/************ Command byte **************************/
+#define CMD_NEXT_SONG     0X01  // Play next song.
+#define CMD_PREV_SONG     0X02  // Play previous song.
+#define CMD_PLAY_W_INDEX  0X03
+#define CMD_VOLUME_UP     0X04
+#define CMD_VOLUME_DOWN   0X05
+#define CMD_SET_VOLUME    0X06
+
+#define CMD_SNG_CYCL_PLAY 0X08  // Single Cycle Play.
+#define CMD_SEL_DEV       0X09
+#define CMD_SLEEP_MODE    0X0A
+#define CMD_WAKE_UP       0X0B
+#define CMD_RESET         0X0C
+#define CMD_PLAY          0X0D
+#define CMD_PAUSE         0X0E
+#define CMD_PLAY_FOLDER_FILE 0X0F
+
+#define CMD_STOP_PLAY     0X16
+#define CMD_FOLDER_CYCLE  0X17
+#define CMD_SHUFFLE_PLAY  0x18 //
+#define CMD_SET_SNGL_CYCL 0X19 // Set single cycle.
+
+#define CMD_SET_DAC 0X1A
+#define DAC_ON  0X00
+#define DAC_OFF 0X01
+
+#define CMD_PLAY_W_VOL    0X22
+#define CMD_PLAYING_N     0x4C
+#define CMD_QUERY_STATUS      0x42
+#define CMD_QUERY_VOLUME      0x43
+#define CMD_QUERY_FLDR_TRACKS 0x4e
+#define CMD_QUERY_TOT_TRACKS  0x48
+#define CMD_QUERY_FLDR_COUNT  0x4f
+
+/************ Opitons **************************/
+#define DEV_TF            0X02
+
+
+/*********************************************************************/
+
+
+void setup() {
+  
+  if (servo_init() == true)
+  Serial.println("Servo init Success");
+  else Serial.println("Failed to init Servo");
+
+  Serial.begin(115200);
+  mp3.begin(9600);
+  delay(500);
+
+  sendCommand(CMD_SEL_DEV, DEV_TF);
+  delay(500);
+
+  
+  sendMP3Command('m');
+  sendMP3Command('1'); //1번 폴더재생 
+}
+
+void loop() {
+  
+  char c = ' ';
+  unsigned long cur_time;
+  unsigned long cur_time2;
+
+
+  accel_val = analogRead(potPin);   // read value(0 ~ 1023) from potentialmeter
+  accel_val = map(accel_val, 0, 1023, MIN, MAX);  // convert potentialmeter read value( 0 ~ 1023) to servo write value( 0 ~ 179 )
+  servo.write(accel_val);           // set servo angle
+  delay(5);     //set margin over 0.05s
+
+  if ( (pre_accel_val - accel_val) > 100 )  {
+    
+    servo.write(179);
+
+    c = '2'; // POPCORN sound play cmd
+    sendMP3Command(c);
+    
+      cur_time = millis();
+     do {
+      Serial.println("Playing 'FOLDER_POPCORN'");
+    } while ( (millis() - cur_time) >= PLAYING_TIME );
+    
+        servo.write(accel_val);
+        c = '1';
+        sendMP3Command(c);
+  }
+
+  if ( accel_val > 170 )
+  {
+    bool counter_fuelcut = false;
+    cur_time2 = millis();
+    while( (millis() - cur_time2) >= FUELCUT_TIME )
+    {
+      if( (pre_accel_val - accel_val) < 5)
+      {
+        counter_fuelcut = true;  
+      }
+      else { counter_fuelcut = false; }
+    }
+    if ( counter_fuelcut == true)
+    {
+      c = '3';
+      sendMP3Command(c);
+      counter_fuelcut = false;
+    }
+  }
+
+  // Check for the answer.
+  if (mp3.available())
+  {
+    Serial.println(decodeMP3Answer());
+  }
+
+  Serial.print("pre_accel_val : ");
+  Serial.println(pre_accel_val);
+  Serial.print("accel_val : ");
+  Serial.println(accel_val);
+
+  Serial.print("change : ");
+  Serial.println(pre_accel_val - accel_val);
+
+  pre_accel_val = accel_val;
+
+   delay(30);
+}
+
+void sendMP3Command(char c) {
+  switch (c) {
+    case '?':
+    case 'h':
+      Serial.println("HELP  ");
+      Serial.println(" p = Play");
+      Serial.println(" P = Pause");
+      Serial.println(" > = Next");
+      Serial.println(" < = Previous");
+      Serial.println(" + = Volume UP");
+      Serial.println(" - = Volume DOWN");
+      Serial.println(" c = Query current file");
+      Serial.println(" q = Query status");
+      Serial.println(" v = Query volume");
+      Serial.println(" x = Query folder count");
+      Serial.println(" t = Query total file count");
+      Serial.println(" 1 = Play folder 1");
+      Serial.println(" 2 = Play folder 2");
+      Serial.println(" 3 = Play folder 3");
+      Serial.println(" 4 = Play folder 4");
+      Serial.println(" 5 = Play folder 5");
+      Serial.println(" S = Sleep");
+      Serial.println(" W = Wake up");
+      Serial.println(" r = Reset");
+      break;
+
+
+    case 'p':
+      Serial.println("Play ");
+      sendCommand(CMD_PLAY, 0);
+      break;
+
+    case 'P':
+      Serial.println("Pause");
+      sendCommand(CMD_PAUSE, 0);
+      break;
+
+    case '>':
+      Serial.println("Next");
+      sendCommand(CMD_NEXT_SONG, 0);
+      sendCommand(CMD_PLAYING_N, 0x0000); // ask for the number of file is playing
+      break;
+
+    case '<':
+      Serial.println("Previous");
+      sendCommand(CMD_PREV_SONG, 0);
+      sendCommand(CMD_PLAYING_N, 0x0000); // ask for the number of file is playing
+      break;
+
+    case '+':
+      Serial.println("Volume Up");
+      sendCommand(CMD_VOLUME_UP, 0);
+      break;
+
+    case '-':
+      Serial.println("Volume Down");
+      sendCommand(CMD_VOLUME_DOWN, 0);
+      break;
+
+    case 'c':
+      Serial.println("Query current file");
+      sendCommand(CMD_PLAYING_N, 0);
+      break;
+
+    case 'q':
+      Serial.println("Query status");
+      sendCommand(CMD_QUERY_STATUS, 0);
+      break;
+
+    case 'v':
+      Serial.println("Query volume");
+      sendCommand(CMD_QUERY_VOLUME, 0);
+      break;
+
+    case 'x':
+      Serial.println("Query folder count");
+      sendCommand(CMD_QUERY_FLDR_COUNT, 0);
+      break;
+
+    case 't':
+      Serial.println("Query total file count");
+      sendCommand(CMD_QUERY_TOT_TRACKS, 0);
+      break;
+
+    case '1':
+      Serial.println("Play folder 1");
+      sendCommand(CMD_FOLDER_CYCLE, 0x0101);
+      break;
+
+    case '2':
+      Serial.println("Play folder 2");
+      sendCommand(CMD_FOLDER_CYCLE, 0x0201);
+      break;
+
+    case '3':
+      Serial.println("Play folder 3");
+      sendCommand(CMD_FOLDER_CYCLE, 0x0301);
+      break;
+
+    case '4':
+      Serial.println("Play folder 4");
+      sendCommand(CMD_FOLDER_CYCLE, 0x0401);
+      break;
+
+    case '5':
+      Serial.println("Play folder 5");
+      sendCommand(CMD_FOLDER_CYCLE, 0x0501);
+      break;
+
+    case 'S':
+      Serial.println("Sleep");
+      sendCommand(CMD_SLEEP_MODE, 0x00);
+      break;
+
+    case 'W':
+      Serial.println("Wake up");
+      sendCommand(CMD_WAKE_UP, 0x00);
+      break;
+
+    case 'r':
+      Serial.println("Reset");
+      sendCommand(CMD_RESET, 0x00);
+      break;
+
+    case 'm':
+      Serial.println("SET VOLUME");
+      sendCommand(CMD_SET_VOLUME, 30);
+      break;
+  }
+}
+
+
+
+/********************************************************************************/
+/*Function decodeMP3Answer: Decode MP3 answer.                                  */
+/*Parameter:-void                                                               */
+/*Return: The                                                  */
+
+String decodeMP3Answer() {
+  String decodedMP3Answer = "";
+
+  decodedMP3Answer += sanswer();
+
+  switch (ansbuf[3]) {
+    case 0x3A:
+      decodedMP3Answer += " -> Memory card inserted.";
+      break;
+
+    case 0x3D:
+      decodedMP3Answer += " -> Completed play num " + String(ansbuf[6], DEC);
+      break;
+
+    case 0x40:
+      decodedMP3Answer += " -> Error";
+      break;
+
+    case 0x41:
+      decodedMP3Answer += " -> Data recived correctly. ";
+      break;
+
+    case 0x42:
+      decodedMP3Answer += " -> Status playing: " + String(ansbuf[6], DEC);
+      break;
+
+    case 0x48:
+      decodedMP3Answer += " -> File count: " + String(ansbuf[6], DEC);
+      break;
+
+    case 0x4C:
+      decodedMP3Answer += " -> Playing: " + String(ansbuf[6], DEC);
+      break;
+
+    case 0x4E:
+      decodedMP3Answer += " -> Folder file count: " + String(ansbuf[6], DEC);
+      break;
+
+    case 0x4F:
+      decodedMP3Answer += " -> Folder count: " + String(ansbuf[6], DEC);
+      break;
+  }
+
+  return decodedMP3Answer;
+}
+
+
+
+
+
+
+/********************************************************************************/
+/*Function: Send command to the MP3                                         */
+/*Parameter:-int8_t command                                                     */
+/*Parameter:-int16_ dat  parameter for the command                              */
+
+void sendCommand(int8_t command, int16_t dat)
+{
+  delay(20);
+  Send_buf[0] = 0x7e;   //
+  Send_buf[1] = 0xff;   //
+  Send_buf[2] = 0x06;   // Len
+  Send_buf[3] = command;//
+  Send_buf[4] = 0x01;   // 0x00 NO, 0x01 feedback
+  Send_buf[5] = (int8_t)(dat >> 8);  //datah
+  Send_buf[6] = (int8_t)(dat);       //datal
+  Send_buf[7] = 0xef;   //
+  Serial.print("Sending: ");
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    mp3.write(Send_buf[i]) ;
+    Serial.print(sbyte2hex(Send_buf[i]));
+  }
+  Serial.println();
+}
+
+
+
+/********************************************************************************/
+/*Function: sbyte2hex. Returns a byte data in HEX format.                 */
+/*Parameter:- uint8_t b. Byte to convert to HEX.                                */
+/*Return: String                                                                */
+
+
+String sbyte2hex(uint8_t b)
+{
+  String shex;
+
+  shex = "0X";
+
+  if (b < 16) shex += "0";
+  shex += String(b, HEX);
+  shex += " ";
+  return shex;
+}
+
+
+
+
+/********************************************************************************/
+/*Function: sanswer. Returns a String answer from mp3 UART module.          */
+/*Parameter:- uint8_t b. void.                                                  */
+/*Return: String. If the answer is well formated answer.                        */
+
+String sanswer(void)
+{
+  uint8_t i = 0;
+  String mp3answer = "";
+
+  // Get only 10 Bytes
+  while (mp3.available() && (i < 10))
+  {
+    uint8_t b = mp3.read();
+    ansbuf[i] = b;
+    i++;
+
+    mp3answer += sbyte2hex(b);
+  }
+
+  // if the answer format is correct.
+  if ((ansbuf[0] == 0x7E) && (ansbuf[9] == 0xEF))
+  {
+    return mp3answer;
+  }
+
+  return "???: " + mp3answer;
+}
+
+bool servo_init()
+{
+  bool servo_status = false;
+  
+  if (servo.attach(9) == true) {
+  servo.write(0);
+  delay(1000);
+  servo.write(179);
+  delay(100);
+  servo.write(0);
+  delay(1000);
+
+  servo_status = true;
+  return servo_status;
+  
+  }
+
+  return servo_status;
+}
